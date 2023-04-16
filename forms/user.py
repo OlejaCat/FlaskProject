ab4c97d163396230e412de3a80781f8ff14bf02a
OlejaCat <118455092+OlejaCat@users.noreply.github.com>
2023-04-16 08:23:38 +0300

    login and register system

--- forms/user.py
+++ forms/user.py
@@ -1,12 +1,12 @@
+from flask_login import UserMixin
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms import PasswordField, StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 
-class RegisterForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    name = StringField('Имя пользователя', validators=[DataRequired()])
-    about = TextAreaField("Немного о себе")
-    submit = SubmitField('Войти')
+class RegisterForm(FlaskForm, UserMixin):
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Confirm Password', validators=[DataRequired()])
+    login = StringField('Login', validators=[DataRequired()])
+    submit = SubmitField('Sign up')
--- main.py
+++ main.py
@@ -1,23 +1,85 @@
+import os
+
 from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_required, login_user, logout_user
+from requests import post
+
+from data import db_session
+from data.users import User
 from forms.LoginForm import LoginForm
+from forms.user import RegisterForm
 
+UPLOAD_FOLDER = '/static/images'
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "yandexlyceum_secret_key"
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-# главная страница
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    """Main page"""
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """Login"""
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Авторизация', form=form)
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/recipes")
+        return render_template('Login.html',
+                               message="Incorrect login or password",
+                               form=form)
+    return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Registration',
+                                   form=form,
+                                   message="Passwords")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Registration',
+                                   form=form,
+                                   message="There is already such a user")
+        """post('http://localhost:5000/api/v2/users', json={
+            'login': form.login.data,
+            'email': form.email.data,
+            'password': form.password.data}).json()
+"""
+        return redirect('/login')
+    return render_template('Registration.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def main():
+    db_session.global_init("db/mars_explorer.db")
+
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
